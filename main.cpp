e0c69172483bd915e5adea7fe24ee86cc3ad43af
Syrovetnyk Bohdan <srovetnyk.bogdan@gmail.com>
2018-06-03 15:40:50 +0300

    Chage file main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 
 int main()
 {
-	std::cout << "Hello Bohdan" << std::endl;
 	std::cout << "Hello world" << std::endl;
 	std::cout << "min: " << min(1,2) << std::endl;
 	std::cout << "max: " << max(5,4) << std::endl;
